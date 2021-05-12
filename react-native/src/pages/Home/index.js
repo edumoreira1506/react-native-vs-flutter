ee516c294284f4a2d66eb6d6ad3a9d76e2fcce8d
Eduardo Moreira <contato@eduardoem.com.br>
2021-05-12 20:11:40 -0300

    Add home page

---
+++ react-native/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+
+export default function HomePage() {
+  return (
+    <View>
+      <StatusBar />
+      <Text>HomePage</Text>
+    </View>
+  )
+}
