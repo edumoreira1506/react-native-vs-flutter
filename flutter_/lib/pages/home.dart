f2fb7a6ae2321e2ee8c433fee19933e38833a0fe
Eduardo Moreira <contato@eduardoem.com.br>
2021-05-14 20:28:09 -0300

    Add items list

--- flutter_/lib/pages/home.dart
+++ flutter_/lib/pages/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_/widgets/button.dart';
-import 'package:flutter_/widgets/input.dart';
+import 'package:flutter_/widgets/item.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -12,45 +11,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController _amountOfItemsController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 30,
-              padding:
-                  EdgeInsets.only(left: 10.0, right: 10.0, top: 2, bottom: 2),
-              margin: EdgeInsets.only(
-                  left: 80.0, right: 80.0, top: 20.0, bottom: 20.0),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(4)),
-                  border: Border.all(color: Colors.black, width: 2)),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Input(
-                      type: TextInputType.number,
-                      controller: _amountOfItemsController,
-                    ),
-                  ),
-                  Button(
-                      onTap: () {
-                        print('tab');
-                      },
-                      text: 'BOTTOM')
-                ],
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-    );
+        body: ListView.builder(
+          padding: EdgeInsets.all(10),
+          itemCount: 5,
+          itemBuilder: (context, int index) {
+            return Item(
+              description: 'Cool description',
+              name: 'I am the item',
+            );
+          },
+        ));
   }
 }
